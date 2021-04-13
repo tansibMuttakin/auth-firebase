400d66fb015b795ac8f0c3be644d51cb345195d8
tansibMuttakin <tansib.muhaimin@gmail.com>
2021-04-13 14:19:54 +0600

    app controller, services,main updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): object {
-    return this.appService.getHello();
+  //get all users
+  @Get('/users')
+  getAllUsers(): object {
+    return this.appService.getAllUsers();
+  }
+
+  // get single user by uid
+  @Get('/user/:uid')
+  getSingleUser(@Param() params): object {
+    return this.appService.getSingleUser(params.uid);
+  }
+
+  //disable single user by id
+  @Get('/user/disable/:uid')
+  disableUser(@Param() params): object {
+    return this.appService.disableUser(params.uid);
+  }
+
+  //enable single user by id
+  @Get('/user/enable/:uid')
+  enableUser(@Param() params): object {
+    return this.appService.enableUser(params.uid);
+  }
+
+  @Get('/user/delete/:uid')
+  deleteUser(@Param() params): object {
+    return this.appService.deleteUser(params.uid);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,37 @@ import * as admin from 'firebase-admin';
 
 @Injectable()
 export class AppService {
-  async getHello(): Promise<object> {
+  async getAllUsers(): Promise<object> {
     let data = {}
-    data =  (await admin.auth().getUserByEmail("tansib.muhaimin@northsouth.edu")).toJSON();
+    data =  (await admin.auth().listUsers());
     return data;  
+  }
+  async getSingleUser(uid:string): Promise<object> {
+    let data = {}
+    data =  ((await admin.auth().getUser(uid)).toJSON());
+    return data;  
+  }
+  async disableUser(uid:string): Promise<object> {
+    let data = {}
+    data =  await admin.auth().updateUser(uid,{
+      disabled: true
+    });
+    return data;  
+  }
+  async enableUser(uid:string): Promise<object> {
+    let data = {}
+    data =  await admin.auth().updateUser(uid,{
+      disabled: false
+    });
+    return data;  
+  }
+  async deleteUser(uid:string): Promise<string> {
+    try {
+      await admin.auth().deleteUser(uid);
+      return 'User deleted successfully';  
+      
+    } catch (error) {
+      return error;
+    }
   }
 }
